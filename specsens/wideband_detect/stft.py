0a087b2a369450c900385158844ada94c5be1684
Fabian Peddinghaus <fabianpedd@gmail.com>
2020-12-08 05:49:45 +0100

    refactor stft

--- specsens/wideband_detect/stft.py
+++ specsens/wideband_detect/stft.py
@@ -32,19 +32,19 @@ class Stft:
 
     def stft(self, sig, f_sample, normalized=True, dB=True):
         '''Compute the stft on a single segment.'''
-        assert len(sig) == self.n, 'Signal segment and fft bin dont match.'
+        assert len(sig) == self.n, 'Signal segment and fft bin length dont match.'
         f, s = self.fft(self.window(sig), f_sample)
         ps = self.psd(s, f_sample, normalized)
         if dB:
-            ps = 10. * np.log10(ps)  # power signal dB
+            ps = 10. * np.log10(ps)  # signalpower in dB
         return f, ps
 
     def spectogram(self, sig, f_sample, normalized=True, dB=True):
         '''Compute spectogram by applying stft on every segment.'''
         tl = []
         xl = []
-        segs = filter(lambda x: len(x) == 1024,
-              [sig[i:i + 1024] for i in range(0, len(sig), 1024)])
+        segs = filter(lambda x: len(x) == self.n,
+                      [sig[i:i + self.n] for i in range(0, len(sig), self.n)])
         for i, seg in enumerate(segs):
             f, x = self.stft(seg, f_sample, normalized, dB)
             tl.append(i * self.n * 1. / f_sample)
