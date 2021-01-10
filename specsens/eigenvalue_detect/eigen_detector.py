1d3b480fc91e33f2ec747c161b17d27b7fa8ca3b
Fabian Peddinghaus <fabianpedd@gmail.com>
2021-01-10 09:59:59 +0100

    rename legacy to archive

--- specsens/eigenvalue_detect/eigen_detector.py
+++ specsens/eigenvalue_detect/eigen_detector.py
@@ -5,6 +5,7 @@ from scipy import linalg
 import tqdm
 
 def corr(x, l, prog_bar=True, w_mat=None):
+    '''Return correlation matrix'''
     x = np.reshape(x, (-1, 1))
     r = np.zeros((l, l), dtype=np.complex128)
     if prog_bar:
@@ -21,21 +22,22 @@ def corr(x, l, prog_bar=True, w_mat=None):
 
 
 def whitening_mat(correlated_noise, l=10, prog_bar=False):
+    '''Return whitenning matrix.'''
     mat_noise_fil = corr(correlated_noise, l=l, prog_bar=prog_bar)
     return np.linalg.inv(linalg.sqrtm(mat_noise_fil))
 
 
 # TODO use eigh since matrix is Hermitian
 
-
-# maximum-minimum eigenvalue
 def mme(x, l=10, w_mat=None):
+    '''Maximum-minimum eigenvalue'''
     Rx = corr(x, l, prog_bar=False, w_mat=w_mat)
     eigvals = np.abs(linalg.eigvals(Rx))
     return np.max(eigvals) / np.min(eigvals)
 
 
 def eme(x, l=10, w_mat=None):
+    '''Energy with minimum eigenvalue'''
     Rx = corr(x, l, prog_bar=False, w_mat=w_mat)
     eigvals = np.abs(linalg.eigvals(Rx))
     energy = np.sum(np.abs(x)**2.) / len(x)
